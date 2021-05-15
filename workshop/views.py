6e62d0c6f3a66cb872a3d0091cf39c4b8e1b13ff
Anindita Som <aninditasom2020@gmail.com>
2021-05-15 22:10:02 +0000

    edit workshop and filtering of workshop, change in view, template and toast messages

--- workshop/views.py
+++ workshop/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
-from .models import Workshop
+from .models import Workshop, Category
 from .forms import WorkshopForm
-
-# from bootstrap_datepicker_plus import DateTimePickerInput
-
-# Create your views here.
-
 
 def add_workshops(request):
     if request.method == 'POST':
@@ -28,22 +23,23 @@ def add_workshops(request):
     }
     return render(request, template, context)
 
-# def edit_workshop(request, workshop_id):
-    
-#     workshop = get_object_or_404(Workshop, pk=workshop_id)
-#     if request.method == 'POST':
-#         form = WorkshopForm(request.POST, request.FILES, instance=workshop)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Successfully updated workshop!')
-#             return redirect(reverse('workshop_detail', args=[workshop.id]))
-#         else:
-#             messages.error(request, 'Failed to update product. Please ensure the form is valid.')
-#     else:
-#         form = WorkshopForm(instance=workshop)
-#         messages.info(request, f'You are editing {workshop.title}')
 
-#     template = 'products/edit_workshops.html'
+# def edit_workshop(request, workshop_id):
+#     workshop = get_object_or_404(Workshop, pk=workshop_id)
+#     # print(workshop)
+#     # if request.method == 'POST':
+#     #     form = WorkshopForm(request.POST, request.FILES, instance=workshop)
+#     #     if form.is_valid():
+#     #         form.save()
+#     #         messages.success(request, 'Successfully updated workshop!')
+#     #         return redirect(reverse('workshop_detail', args=[workshop.id]))
+#     #     else:
+#     #         messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+#     # else:
+#     form = WorkshopForm(instance=workshop)
+#     messages.info(request, f'You are editing {workshop.title}')
+
+#     template = 'workshop/edit_workshop.html'
 #     context = {
 #         'form': form,
 #         'workshop': workshop,
@@ -55,12 +51,20 @@ def add_workshops(request):
 def all_workshops(request):
     
     workshops = Workshop.objects.all()
-    
+    query = None
+    categories = None
+    template = 'workshop/workshops.html'
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            workshops = workshops.filter(category_name__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
     context = {
         'workshops': workshops,
+        'current_categories': categories,
     }
 
-    return render(request, 'workshop/workshops.html', context)
+    return render(request, template, context)
 
 
 def workshop_details(request, workshop_id):
@@ -74,7 +78,22 @@ def workshop_details(request, workshop_id):
 
 def edit_workshop(request, workshop_id):
     workshop = get_object_or_404(Workshop, pk=workshop_id)
+    if request.method == 'POST':
+        form = WorkshopForm(request.POST, request.FILES, instance=workshop)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated workshop!')
+            return redirect(reverse('workshop_details', args=[workshop.id]))
+        else:
+            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+    else:
+        form = WorkshopForm(instance=workshop)
+    
+    messages.info(request, f'You are editing {workshop.title}')
+    template = 'workshop/edit_workshop.html'
+    
     context = {
-        'workshop': workshop
+        'form': form,
+        'workshop': workshop,
     }
-    return render(request, 'workshop/edit_workshop.html', context)
+    return render(request, template , context)

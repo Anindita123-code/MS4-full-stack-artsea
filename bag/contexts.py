bcac632c71bac846a7fead9f1edb36a1ea1dc3ca
Anindita Som <aninditasom2020@gmail.com>
2021-05-18 00:35:55 +0000

    items added to the shopping bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,11 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from workshop.models import Workshop
+from django.shortcuts import get_object_or_404
 
 
 def bag_contents(request):
     bag_items = []
     total = 0
     workshop_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+
+        workshop = get_object_or_404(Workshop, pk=3)
+        total += quantity * workshop.price
+        workshop_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'workshop': workshop,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)

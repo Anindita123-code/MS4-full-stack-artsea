3d1f40aa2999f5355335c44f66f2a3f30e391319
Anindita Som <aninditasom2020@gmail.com>
2021-05-20 15:18:04 +0000

    secure checkout form, view, url and checkout css

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+from workshop.models import Workshop
 
 
 def view_bag(request):
@@ -8,28 +8,37 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
+    workshop = get_object_or_404(Workshop, pk=item_id)
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
     bag = request.session.get('bag', {})
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(
+            request, f'Updated {workshop.title} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {workshop.title} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
 
 
 def adjust_bag(request, item_id):
+    workshop = get_object_or_404(Workshop, pk=item_id)
     quantity = int(request.POST.get('quantity'))
 
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(
+            request, f'Updated {workshop.title} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {workshop.title} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -37,13 +46,15 @@ def adjust_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     try:
+        workshop = get_object_or_404(Workshop, pk=item_id)
         bag = request.session.get('bag', {})
 
         bag.pop(item_id)
-
+        messages.success(request, f'Removed {workshop.title} from your bag')
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item {e}')
         return HttpResponse(status=500)
 
--- checkout/forms.py
+++ checkout/forms.py
@@ -5,8 +5,8 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('full_name', 'email', 'phone_number',
-                  'street_address1', 'street_address2',
-                  'town_or_city', 'postcode', 'country',)
+                #   'street_address1', 'street_address2','postcode',
+                  'town_or_city', 'country',)
 
         def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
@@ -14,10 +14,10 @@ class OrderForm(forms.ModelForm):
                 'full_name': 'Full Name',
                 'email': 'Email Address',
                 'phone_number': 'Phone Number',
-                'postcode': 'Postal Code',
+                # 'postcode': 'Postal Code',
                 'town_or_city': 'Town or City',
-                'street_address1': 'Street Address 1',
-                'street_address2': 'Street Address 2',
+                # 'street_address1': 'Street Address 1',
+                # 'street_address2': 'Street Address 2',
             }
 
             self.fields['full_name'].widget.attrs['autofocus'] = True

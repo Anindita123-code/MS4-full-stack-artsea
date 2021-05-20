962f0708f552c8959126702c23b918fa301d2929
Anindita Som <aninditasom2020@gmail.com>
2021-05-20 10:40:12 +0000

    checkout model, signal, view and url coding

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
-# Create your views here.
+from .forms import OrderForm
+from .models import Order, OrderLineItem
+from django.contrib import messages
+
+
+def checkout(request):
+    bag = request.session.get('bag',{})
+    if not bag:
+        messages.error(request, "Currently there are no items in your bag")
+        return redirect(reverse('workshops'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+    return render(request, template, context)

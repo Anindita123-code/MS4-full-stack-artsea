a76e4493dc3703f9691fe861d4380aa2b83ed993
Anindita Som <aninditasom2020@gmail.com>
2021-05-21 16:19:55 +0000

    added django-countries for stripe alignment with country code

--- checkout/models.py
+++ checkout/models.py
@@ -5,14 +5,14 @@ from django.db.models import Sum
 from django.conf import settings
 
 from workshop.models import Workshop
-
+from django_countries.fields import CountryField
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     date = models.DateTimeField(auto_now_add=True)
     delivery_cost = models.DecimalField(max_digits=6, decimal_places=2,

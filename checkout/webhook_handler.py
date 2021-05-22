df48dc7b7cfc601a3387c0339a39fd5533a67eb6
Anindita Som <aninditasom2020@gmail.com>
2021-05-22 16:37:42 +0000

    order confirmation email coding

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,6 +1,9 @@
 
 from django.http import HttpResponse
-# from django.conf import settings
+
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from .models import Order, OrderLineItem
 from workshop.models import Workshop
@@ -24,6 +27,25 @@ class StripeWH_Handler:
             content=f'Unhandled Webhook received: {event["type"]}',
             status=200)
 
+    def _send_confirmation_email(self, order):
+        """Send the user a confirmation email"""
+        cust_email = order.email
+
+        print(cust_email)
+        
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order})
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+        
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [cust_email]
+        )        
     def handle_payment_intent_payment_failed(self, event):
         """
         Handle the payment_intent.payment_failed webhook from Stripe
@@ -75,7 +97,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
-            # self._send_confirmation_email(order)
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200)
@@ -101,15 +123,15 @@ class StripeWH_Handler:
                             quantity=item_data,
                         )
                         order_line_item.save()
+           
             except Exception as e:
                 if order:
                     order.delete()
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | ERROR: {e}',
                     status=500)
-       
-        # self._send_confirmation_email(or
-        
+
+        self._send_confirmation_email(order)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}|SUCCESS:Created order in webhook',
             status=200)

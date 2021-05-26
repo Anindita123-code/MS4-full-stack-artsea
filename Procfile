115574172a7ce1686d064bf01b23efbd52ad6e52
Anindita Som <aninditasom2020@gmail.com>
2021-05-26 10:55:39 +0000

    d2: added Procfile, DISABLE_COLLECTSTATIC to 1 and added heroku link to ALLOWED_HOST in settings.py

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn artsea.wsgi:application
--- artsea/settings.py
+++ artsea/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-#85z8yiga&ei6=&an80*jsv603%$uh+xytgk7hjg1g+w=9fyp7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anindita-artsea.herokuapp.com', 'localhost']
 
 
 
@@ -121,12 +121,18 @@ WSGI_APPLICATION = 'artsea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ['DATABASE_URL'])
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators

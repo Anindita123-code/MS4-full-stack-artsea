d8826f4c5ffe0a17d420c9df06d1df84f24ea2c5
Anindita Som <aninditasom2020@gmail.com>
2021-05-26 11:10:22 +0000

    change settings.py to read the secret key from environment var and changed debug

--- artsea/settings.py
+++ artsea/settings.py
@@ -22,14 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#85z8yiga&ei6=&an80*jsv603%$uh+xytgk7hjg1g+w=9fyp7'
+
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['anindita-artsea.herokuapp.com', 'localhost']
-
-
 
 # Application definition
 
